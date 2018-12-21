91a06459ce1abbb8d5503b09957ebf6457043bff
hcoder <hcoder.cn@gmail.com>
2018-12-21 16:29:39 +0800

    test

--- server.go
+++ server.go
@@ -9,7 +9,11 @@ func main() {
 	fmt.Print("你好 go")
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	fmt.Println(router)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	//router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
 	//router.GET("/users", apis.GetUserAPI)
 	//router.POST("/user", apis.AddPersonAPI)
