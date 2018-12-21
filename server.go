4c42f8b00ee1f1b782ce6f15feaf9ded1c6e25a7
hcoder <hcoder.cn@gmail.com>
2018-12-21 16:27:02 +0800

    test

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"goLearning/apis"
 )
 
 func main() {
@@ -9,7 +10,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	fmt.Println(router)
-	//router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
+	router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
 	//router.GET("/users", apis.GetUserAPI)
 	//router.POST("/user", apis.AddPersonAPI)
 	//router.Run(":8080")
