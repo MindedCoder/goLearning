de9a918af48735a8613e57dbc3fcdf33fd6eda09
hcoder <hcoder.cn@gmail.com>
2018-12-21 16:01:29 +0800

    add console

--- server.go
+++ server.go
@@ -2,13 +2,11 @@ package main
 import (
 	"goLearning/router"
 	"gopkg.in/mgo.v2"
-	"fmt"
 )
 
 func main() {
 	mgo.SetDebug(true)
 	mgo.SetStats(true)
 	rt := router.InitRouter()
-	fmt.Println("Listening and serving HTTP on :8080")
-	rt.Run(":8080")
+	rt.Run(":5000")
 }
