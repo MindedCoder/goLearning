2842a21b6f786957468cab685840c7921c659267
hcoder <hcoder.cn@gmail.com>
2018-12-21 16:31:33 +0800

    test

--- server.go
+++ server.go
@@ -2,19 +2,14 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	//"goLearning/apis"
+	."goLearning/apis"
 )
 
 func main() {
 	fmt.Print("你好 go")
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	//router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
+	router.OPTIONS("/1.1/functions/12kmCollectStatDatas", DefaultAPI)
 	//router.GET("/users", apis.GetUserAPI)
 	//router.POST("/user", apis.AddPersonAPI)
 	router.Run(":8080")
