b0a838048adf27ef82c72db394abb60251da1a6d
hcoder <hcoder.cn@gmail.com>
2018-12-21 17:51:01 +0800

    add cors

--- router/web.go
+++ router/web.go
@@ -3,10 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"goLearning/apis"
+	"goLearning/utils"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(utils.Cors())
 	router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
 	router.GET("/users", apis.GetUserAPI)
 	router.POST("/user", apis.AddPersonAPI)
