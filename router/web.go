b7b9b4d0a868bb9dbb49d924c178c37bbd16a475
hcoder <hcoder.cn@gmail.com>
2018-12-25 15:07:36 +0800

    update test

--- router/web.go
+++ router/web.go
@@ -10,7 +10,7 @@ func InitRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(utils.Cors())
 	router.OPTIONS("/1.1/functions/12kmCollectStatDatas", apis.DefaultAPI)
-	router.GET("/1.1/classes/_User", apis.QueryUsers)
+	router.OPTIONS("/1.1/classes/_User", apis.QueryUsers)
 	router.POST("/user", apis.AddPersonAPI)
 	return  router
 }
