ef54126bfc72d5b06278430ee1e8a0daaf03dc66
hcoder <hcoder.cn@gmail.com>
2019-01-08 15:58:13 +0800

    deploy a new staging version

--- utils/Cors.go
+++ utils/Cors.go
@@ -26,7 +26,7 @@ func Cors() gin.HandlerFunc {
 			//下面的都是乱添加的-_-~
 			// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Origin", "*")
-			c.Header("Access-Control-Allow-Headers", headerStr)
+			c.Header("Access-Control-Allow-Headers", "*")
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			// c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
